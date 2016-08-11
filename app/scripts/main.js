5224106c1707cad74ed32a402675261c80c2cf77
Nelson Páez <nelpa90@gmail.com>
2016-08-11 02:57:20 -0400

    - Mejoras para el JSHint en main.js - Reemplazado el alert por el modal de bootstrap

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,114 +1,130 @@
-var jugadores = ['x','o'];
-var indiceJugadorActual;
-var jugadorActual = function(){
-  return jugadores[indiceJugadorActual];
-};
-var siguienteJugador = function(){
-  if(controlarGanador()){
-    return;
-  }
+/*globals $:false */
 
-  if(indiceJugadorActual<1){
-    indiceJugadorActual++;
-  }else{
-    indiceJugadorActual = 0;
-  }
+(function() {
 
-  $('#titulo').html(mensajeTurno(jugadorActual()));
-};
+  'use strict';
 
-var mensajeTurno = function(jugador){
-  return 'Turno de <b class="turno">' + jugador + '</b>';
-};
+  var jugadores = ['x', 'o'];
+  var indiceJugadorActual;
 
-var controlarGanador = function(){
-  // Control de filas
-  var hayGanador = false;
-  $('table.tateti tr').each(function(){
-    hayGanador = hayGanador || $(this).find('[data-jugador='+jugadorActual()+']').length == 3;
-  });
+  var jugadorActual = function(){
+    return jugadores[indiceJugadorActual];
+  };
 
-  if(!hayGanador){
-    // Control de columnas
-    for(var i=1;i<=3;i++){
-      hayGanador = hayGanador || $('table.tateti td[data-columna='+i+'][data-jugador='+jugadorActual()+']').length == 3;
+
+  var mensajeTurno = function(jugador){
+    return 'Turno de <b class="turno">' + jugador + '</b>';
+  };
+
+  var mensajeModal = function(mensaje){
+    $('#modal-mensajes').find('[data-mensaje]').html(mensaje);
+    $('#modal-mensajes').modal('show');
+  };
+
+  var controlarGanador = function(){
+    // Control de filas
+    var hayGanador = false;
+    $('table.tateti tr').each(function(){
+      hayGanador = hayGanador || $(this).find('[data-jugador=' + jugadorActual() + ']').length === 3;
+    });
+
+    if(!hayGanador){
+      // Control de columnas
+      for(var i = 1; i <= 3; i++){
+        hayGanador = hayGanador || $('table.tateti td[data-columna=' + i + '][data-jugador=' + jugadorActual() + ']').length === 3;
+      }
     }
-  }
 
-  if(!hayGanador){
-    // Control de diagonal principal
-    var contador = 0;
-    for(var i=1;i<=3;i++){
-      contador += $('table.tateti tr[data-fila='+i+'] td[data-columna='+i+'][data-jugador='+jugadorActual()+']').length;
+    if(!hayGanador){
+      // Control de diagonal principal
+      var matrizPrincipalMarcada = 0;
+      for(var j = 1; j <= 3; j++){
+        matrizPrincipalMarcada += $('table.tateti tr[data-fila=' + j + '] td[data-columna=' + j + '][data-jugador=' + jugadorActual() + ']').length;
+      }
+      hayGanador = matrizPrincipalMarcada === 3;
     }
-    hayGanador = contador==3;
-  }
 
-  if(!hayGanador){
-    // Control de diagonal secundaria
-    var contador = 0;
-    for(var i=1;i<=3;i++){
-      contador += $('table.tateti tr[data-fila='+i+'] td[data-columna='+(4-i)+'][data-jugador='+jugadorActual()+']').length;
+    if(!hayGanador){
+      // Control de diagonal secundaria
+      var matrizInvertidaMarcada = 0;
+      for(var k = 1; k <= 3; k++){
+        matrizInvertidaMarcada += $('table.tateti tr[data-fila=' + k + '] td[data-columna=' + (4 - k) + '][data-jugador=' + jugadorActual() + ']').length;
+      }
+      hayGanador = matrizInvertidaMarcada === 3;
     }
-    hayGanador = contador==3;
-  }
 
-  var casillasJugadas = $('[data-jugador]').length;
-  if(hayGanador){
-    $('table.tateti').find('td').unbind();
-    alert('¡' + jugadorActual().toUpperCase() + ' ha ganado con '
-      + casillasJugadas + ' casillas jugadas!');
-    $('#titulo').html('Ganó ' + jugadorActual().toUpperCase());
-  }else if(casillasJugadas==9){
-    alert('¡Ha sido un empate!')
-    $('#titulo').html('Empate :-|');
-    hayGanador = true;
-  }
+    var casillasJugadas = $('[data-jugador]').length;
+    if(hayGanador){
+      $('table.tateti').find('td').unbind();
+      mensajeModal('¡' + jugadorActual().toUpperCase() + ' ha ganado con '
+        + casillasJugadas + ' casillas jugadas!');
+      $('#titulo').html('Ganó ' + jugadorActual().toUpperCase());
+    }else if(casillasJugadas === 9){
+      mensajeModal('¡Ha sido un empate!');
+      $('#titulo').html('Empate :-|');
+      hayGanador = true;
+    }
 
-  return hayGanador;
-}
+    return hayGanador;
+  };
 
-var jugar = function(){
-  indiceJugadorActual = -1;
-  $('#contenedor').empty();
-  var tablero = $('#tablero-ejemplo').clone();
-  tablero.removeAttr('id');
-  tablero.appendTo('#contenedor');
-  tablero.hide();
-  tablero.removeClass('hidden');
-  tablero.slideDown('fast',function(){
-    $(window).trigger('resize');
-  });
-  $('[data-jugar]').html('Empezar de nuevo');
-  siguienteJugador();
-
-  tablero.find('td').click(function(){
-    var celda = $(this);
-    if(celda.is('[data-jugador]')){
+  var siguienteJugador = function(){
+    if(controlarGanador()){
       return;
     }
-    celda.attr('data-jugador',jugadorActual());
-    celda.html(jugadorActual());
+
+    if(indiceJugadorActual < 1){
+      indiceJugadorActual++;
+    }else{
+      indiceJugadorActual = 0;
+    }
+
+    $('#titulo').html(mensajeTurno(jugadorActual()));
+  };
+
+  var jugar = function(){
+    indiceJugadorActual = -1;
+    $('#contenedor').empty();
+    var tablero = $('#tablero-ejemplo').clone();
+    tablero.removeAttr('id');
+    tablero.appendTo('#contenedor');
+    tablero.hide();
+    tablero.removeClass('hidden');
+    tablero.slideDown('fast', function(){
+      $(window).trigger('resize');
+    });
+    $('[data-jugar]').html('Empezar de nuevo');
     siguienteJugador();
-  });
-};
 
-$(document).ready(function(){
+    tablero.find('td').click(function(){
+      var celda = $(this);
+      if(celda.is('[data-jugador]')){
+        return;
+      }
+      celda.attr('data-jugador', jugadorActual());
+      celda.html(jugadorActual());
+      siguienteJugador();
+    });
+  };
 
-  // Tablero Responsive
-  $(window).on('resize',function(){
-    $('table.tateti td').each(function(){
-      $(this).removeAttr('style');
-      var ancho = $('#contenedor').width() / 3;
-      var alto = ancho;
-      $(this).css({
-        width: ancho,
-        height: alto
+  $(document).ready(function(){
+
+    // Tablero Responsive
+    $(window).on('resize', function(){
+      $('table.tateti td').each(function(){
+        $(this).removeAttr('style');
+        var ancho = $('#contenedor').width() / 3;
+        var alto = ancho;
+        $(this).css({
+          width: ancho,
+          height: alto
+        });
       });
     });
+
+    // Damos acciones a los botones que tienen el atributo data-jugar
+    $('[data-jugar]').click(jugar);
+
   });
 
-  // Damos acciones a los botones que tienen el atributo data-jugar
-  $('[data-jugar]').click(jugar);
-
-});
+}());
